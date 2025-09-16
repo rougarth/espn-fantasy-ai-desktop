4027f1025e9c2d8ce250615d23b656df314095b1
rougarth <gbrielmelo0@gmail.com>
2025-09-16 16:56:30 -0400

    Update preload.js

--- src/preload.js
+++ src/preload.js
@@ -1,11 +1,13 @@
-
 const { contextBridge, ipcRenderer } = require('electron');
+
 contextBridge.exposeInMainWorld('espn', {
   login: () => ipcRenderer.invoke('espn:login'),
   getStatus: () => ipcRenderer.invoke('espn:status'),
   getLeagues: () => ipcRenderer.invoke('espn:getLeagues'),
   getTeams: (leagueId) => ipcRenderer.invoke('espn:getTeams', leagueId),
   getStandings: (leagueId) => ipcRenderer.invoke('espn:getStandings', leagueId),
-  getMatchups: (leagueId, scoringPeriodId) => ipcRenderer.invoke('espn:getMatchups', { leagueId, scoringPeriodId }),
-  getRoster: (leagueId, teamId, scoringPeriodId) => ipcRenderer.invoke('espn:getRoster', { leagueId, teamId, scoringPeriodId }),
+  getMatchups: (leagueId, scoringPeriodId) =>
+    ipcRenderer.invoke('espn:getMatchups', { leagueId, scoringPeriodId }),
+  getRoster: (leagueId, teamId, scoringPeriodId) =>
+    ipcRenderer.invoke('espn:getRoster', { leagueId, teamId, scoringPeriodId })
 });
